8b6c3ff3b4ba4c139f9191f45de8e29b479c91e6
nine-chairs <havlik9@gmail.com>
2023-11-04 18:57:55 +0100

    created the basic page structure and a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'
+// pages
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Services.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+        <br />
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At recusandae quae pariatur expedita est in odio fugiat rem mollitia ullam, consequatur quidem vero. Doloremque velit provident, totam corporis iusto similique, repellat quaerat laborum asperiores voluptas non harum nam quis maxime voluptates quo tempora dicta illum tempore exercitationem, numquam molestias. Nam quod porro omnis nulla error, dolores autem. Iste voluptates aliquam amet enim voluptate deserunt corporis esse itaque atque incidunt, ipsam aspernatur mollitia pariatur dolorum nam eius deleniti fugiat in facere provident perspiciatis. Tempora, quidem! Id a voluptas aliquid vitae numquam officiis distinctio, atque animi sequi, tenetur quis, dicta iste nostrum!
+        </div>
+    </div>
+  )
+}
+
+export default Services
