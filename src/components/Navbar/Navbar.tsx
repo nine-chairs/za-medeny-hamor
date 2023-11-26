d8d8683ff01bdd0cd433996d426a438d14797e4d
nine-chairs <havlik9@gmail.com>
2023-11-26 13:21:46 +0100

    updated styling of the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { useTranslation } from "react-i18next"
+import logo from '../../icons/logo_c.svg'
 import './Navbar.css'
 import LanguageSelector from '../LanguageSelector/LanguageSelector'
 
@@ -9,9 +10,9 @@ const Navbar = () => {
   const { t } = useTranslation()
   return (
     <nav>
-      <div className='logo'>
+      <div className='logoContainer'>
         <Link to='/' className='title'>
-          za! medený hámor
+          <img className='logo' src={logo} alt={'logo'} />
         </Link>
       </div>
       <div
@@ -24,25 +25,28 @@ const Navbar = () => {
         <span></span>
         <span></span>
       </div>
-      <ul className={menuOpen ? 'open' : ''}>
-        <li>
-          <NavLink to='/about'>{t('title_about')}</NavLink>
-        </li>
-        <li>
-          <NavLink to='/medeny_hamor'>{t('title_medeny_hamor')}</NavLink>
-        </li>
-        <li>
-          <NavLink to='/news'>{t('title_news')}</NavLink>
-        </li>
-        <li>
-          <NavLink to='/support'>{t('title_support')}</NavLink>
-        </li>
-        <li>
-          <NavLink to='/contact'>{t('title_contact')}</NavLink>
-        </li>
-      </ul>
-      <div className="vertical-line"></div>
-      <LanguageSelector />
+      <div className='navItemsContainer'>
+        <ul className={menuOpen ? 'open' : ''}>
+          <li>
+            <NavLink to='/about'>{t('title_about')}</NavLink>
+          </li>
+          <li>
+            <NavLink to='/medeny_hamor'>{t('title_medeny_hamor')}</NavLink>
+          </li>
+          <li>
+            <NavLink to='/news'>{t('title_news')}</NavLink>
+          </li>
+          <li>
+            <NavLink to='/support'>{t('title_support')}</NavLink>
+          </li>
+          <li>
+            <NavLink to='/contact'>{t('title_contact')}</NavLink>
+          </li>
+        </ul>
+        <div className="vertical-line"></div>
+        <LanguageSelector />
+      </div>
+
     </nav>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import photo from '../icons/hamor-fotka.jpg'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+
+      <div className='photoContainer'>
+        <img className='photo' src={photo} alt='hamor v pozadi' />
+
+      </div>
+
     </div>
   )
 }
