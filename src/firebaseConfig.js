51cbeddf4d6de17383036b1dbea5b9fbf647f54f
nine-chairs <havlik9@gmail.com>
2023-12-06 18:44:33 +0100

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFBiwy6mnro2DDsXwSrS7wa1g_lfdBzas",
+  authDomain: "za-medeny-hamor.firebaseapp.com",
+  projectId: "za-medeny-hamor",
+  storageBucket: "za-medeny-hamor.appspot.com",
+  messagingSenderId: "1011691942346",
+  appId: "1:1011691942346:web:a2927e7b46a13a43029f33",
+  measurementId: "G-BEXL20M1J1"
+}
