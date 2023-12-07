02007feb03b0f202ddb97dcf8098e4ced41ca44e
nine-chairs <havlik9@gmail.com>
2023-12-07 16:59:01 +0100

    connected the photo gallery with firebase

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,90 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { storage } from '../../firebase';
-import { ref, listAll, getDownloadURL, StorageReference } from 'firebase/storage';
-import './ImageSlider.css'; // Import the CSS file
+import React, { useState, useEffect } from 'react'
+import './ImageSlider.css' // Import the CSS file
 
 interface ImageSliderProps {
-  images: string[]; // Paths to the images
+  images: string[] // Paths to the images
 }
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [isPaused, setIsPaused] = useState<boolean>(false);
-  const [imageUrls, setImageUrls] = useState<string[]>([]);
-  const [areImagesLoaded, setAreImagesLoaded] = useState<boolean>(false);
-
-  const imagesListRef = ref(storage, 'images/');
-
-  useEffect(() => {
-    const fetchImageUrls = async () => {
-      try {
-        const response = await listAll(imagesListRef);
-        const urls = await Promise.all(
-          response.items.map(async (item: StorageReference) => {
-            const url = await getDownloadURL(item);
-            return url;
-          })
-        );
-        setImageUrls(urls);
-        setAreImagesLoaded(true); // Mark images as loaded
-      } catch (error) {
-        console.error('Error fetching image URLs:', error);
-      }
-    };
-
-    fetchImageUrls();
-  }, [imagesListRef]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [isPaused, setIsPaused] = useState<boolean>(false)
 
   // Function to go to the next slide
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
-  };
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+  }
 
   // Function to go to the previous slide
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length);
-  };
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
+  }
 
   // Auto advance to the next slide every 2 seconds
   useEffect(() => {
-    let intervalId: NodeJS.Timeout;
+    let intervalId: NodeJS.Timeout
 
-    if (!isPaused && areImagesLoaded) {
+    if (!isPaused) {
       intervalId = setInterval(() => {
-        nextSlide();
-      }, 2000);
+        nextSlide()
+      }, 2000)
     }
 
     // Clear the interval when the component is unmounted or paused
-    return () => clearInterval(intervalId);
-  }, [currentIndex, imageUrls.length, isPaused, areImagesLoaded]);
+    return () => clearInterval(intervalId)
+  }, [currentIndex, images.length, isPaused])
 
   // Function to toggle pause/resume
   const togglePause = () => {
-    setIsPaused((prevPaused) => !prevPaused);
-  };
+    setIsPaused((prevPaused) => !prevPaused)
+  }
 
   return (
     <div>
-      {areImagesLoaded && (
+      {images.length > 0 && (
         <div className="image-slider">
-          <button className="left-arrow" onClick={prevSlide}>&#9664;</button>
+          <button className="left-arrow" onClick={prevSlide}>
+            &#9664
+          </button>
 
           <img
-            src={imageUrls[currentIndex]}
+            src={images[currentIndex]}
             alt={`Slide ${currentIndex}`}
             loading="lazy" // Apply lazy loading
           />
 
-          <button className="right-arrow" onClick={nextSlide}>&#9654;</button>
+          <button className="right-arrow" onClick={nextSlide}>
+            &#9654
+          </button>
           <button className="pause-resume" onClick={togglePause}>
             {isPaused ? 'Resume' : 'Pause'}
           </button>
         </div>
       )}
-      {!areImagesLoaded && <p>Loading images...</p>}
+      {images.length === 0 && <p>No images to display.</p>}
     </div>
-  );
+  )
 }
 
-export default ImageSlider;
+export default ImageSlider
--- src/pages/PhotoGallery/PhotoGallery.tsx
+++ src/pages/PhotoGallery/PhotoGallery.tsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import { useTranslation } from "react-i18next"
+import React, { useState, useEffect } from 'react'
+import { storage } from '../../firebase'
+import { ref, listAll, getDownloadURL, StorageReference } from 'firebase/storage'
+import { useTranslation } from 'react-i18next'
+import './PhotoGallery.css' // Import your CSS file for styling
 
-const PhotoGallery = () => {
+const PhotoGallery: React.FC = () => {
   const { t } = useTranslation()
+  const [imageUrls, setImageUrls] = useState<string[]>([])
+  const [areImagesLoaded, setAreImagesLoaded] = useState<boolean>(false)
+
+  const imagesListRef = ref(storage, 'images/')
+
+  useEffect(() => {
+    const fetchImageUrls = async () => {
+      try {
+        const response = await listAll(imagesListRef)
+        const urls = await Promise.all(
+          response.items.map(async (item: StorageReference) => {
+            const url = await getDownloadURL(item)
+            return url
+          })
+        )
+        setImageUrls(urls)
+        setAreImagesLoaded(true) // Mark images as loaded
+      } catch (error) {
+        console.error('Error fetching image URLs:', error)
+      }
+    }
+
+    fetchImageUrls()
+  }, [imagesListRef])
+
   return (
-    <h1>{t('title_photo_gallery')}</h1>
+    <div>
+      <h1>{t('title_photo_gallery')}</h1>
+      {areImagesLoaded ? (
+        <div className="photo-grid">
+          {imageUrls.map((url, index) => (
+            <img
+              key={index}
+              src={url}
+              alt={`Photo ${index}`}
+              className="photo-item"
+              loading="lazy" // Add lazy loading attribute
+            />
+          ))}
+        </div>
+      ) : (
+        <p>Loading images...</p>
+      )}
+    </div>
   )
 }
 
